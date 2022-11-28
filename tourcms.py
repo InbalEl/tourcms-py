27844f904a6326ced8d85077400315dce8612c1e
InbalEl <elmaleh.inbal@gmail.com>
2022-11-28 09:17:52 +0200

    prep for adding all missing functions

--- tourcms.py
+++ tourcms.py
@@ -1,49 +1,49 @@
+import datetime as dt
+import time
+import calendar
+import urllib
+import xmltodict
 import hmac
 import hashlib
-import datetime as dt
-import calendar
-try: # Python 3
-  import urllib.parse as urllib
-except ImportError:
-  import urllib
-try: # Python 3
-  import urllib.request as urllib2
-except ImportError: 
-    import urllib2
-try:
-  import xmltodict
-except ImportError:
-  pass
-import time
 import base64
-import logging
 
-
-__author__ = 'Jonathan Harrington'
-__version__ = '0.3'
+__author__ = 'Inbal Elmaleh'
+__version__ = '1.0'
 __license__ = 'BSD'
 
+# self:
+# marketp_id(int)
+# private_key
+# result_type
+# base_url
 
 class Connection(object):
-  def __init__(self, marketp_id, private_key, result_type = "raw", loglevel = logging.CRITICAL):
+  def __init__(self, marketp_id, private_key, result_type = "raw"):
     try:
       int(marketp_id)
     except ValueError:
       raise TypeError("Marketplace ID must be an Integer")
+
     self.marketp_id = int(marketp_id)
     self.private_key = private_key
     self.result_type = result_type
     self.base_url = "https://api.tourcms.com"
-    self.logger = logging.getLogger("tourcms")
-    self.logger.addHandler(logging.StreamHandler())
-    self.logger.setLevel(loglevel)
-    
+
   def _generate_signature(self, path, verb, channel, outbound_time):
-    string_to_sign = u"{0}/{1}/{2}/{3}{4}".format(channel, self.marketp_id, verb, outbound_time, path)
-    self.logger.debug("string_to_sign is: {0}".format(string_to_sign))
-    dig = hmac.new(self.private_key.encode('utf8'), string_to_sign.encode('utf8'), hashlib.sha256)
-    b64 = base64.b64encode(dig.digest())
-    return urllib.quote_plus(b64)
+        
+    string_to_sign = f"{channel}/{self.marketp_id}/{verb}/{outbound_time}{path}"
+    print(string_to_sign)
+
+    # FROM THE WORKING JS LIB:
+    # var hash = crypto.createHmac('SHA256', apiKey).update(stringToSign).digest('base64');
+    # var signature = this.rawurlencode(hash);
+
+    dig = hmac.new(self.private_key.encode('utf8'),
+                   string_to_sign.encode('utf8'),
+                   hashlib.sha256)
+    b64 = base64.b64encode(dig.digest()) # b64encode takes bytes, not str
+
+    return urllib.parse.quote_plus(b64)
 
   def _response_to_native(self, response):
     try:
@@ -51,45 +51,77 @@ class Connection(object):
     except KeyError:
       return xmltodict.parse(response)
     except NameError:
-      self.logger.error("XMLtodict not available, install it by running\n\t$ pip install xmltodict\n")
       return response
 
   def _request(self, path, channel = 0, params = {}, verb = "GET"):
-    url = self.base_url + path + "?" + urllib.urlencode(params)
-    self.logger.debug("url is: {0}".format(url))
+    
+    path_with_params = path + "?" + urllib.parse.urlencode(params)
+    url = self.base_url + path + "?" + urllib.parse.urlencode(params)
     req_time = dt.datetime.utcnow()
+    
     signature = self._generate_signature(
-      path + "?" + urllib.urlencode(params), verb, channel, int(time.mktime(calendar.timegm(req_time.timetuple())))
-    )    
+      path_with_params , verb, channel,int(calendar.timegm(req_time.timetuple()))
+    )
+    
     headers = {
-      "Content-type": "text/xml", 
+      "Content-type": "text/xml",
       "charset": "utf-8", 
       "x-tourcms-date": req_time.strftime("%a, %d %b %Y %H:%M:%S GMT"), 
-      "Authorization": "TourCMS {0}:{1}:{2}".format(channel, self.marketp_id, signature)
+      "Authorization": f"TourCMS {channel}:{self.marketp_id}:{signature}"
     }
-    self.logger.debug("Headers are: {0}".format(", ".join(["{0} => {1}".format(k,v) 
-                                                           for k,v in headers.items()])))
-    req = urllib2.Request(url)
+
+    print(f"Content-type: {headers['Content-type']}")
+    print(f"x-tourcms-date: {headers['x-tourcms-date']}")
+    print(f"Authorization: {headers['Authorization']}")
+
+    req = urllib.request.Request(url)
+    
     for key, value in headers.items():
       req.add_header(key, value)
-    response = urllib2.urlopen(req).read()
+    
+    response = urllib.request.urlopen(req).read()
+
     return response if self.result_type == "raw" else self._response_to_native(response)
 
+
+  # HOUSEKEEPING
   def api_rate_limit_status(self, channel = 0):
     return self._request("/api/rate_limit_status.xml", channel)
-  
+
+  # CHANNELS  
   def list_channels(self):
     return self._request("/p/channels/list.xml")
   
   def show_channel(self, channel):
     return self._request("/c/channel/show.xml", channel)
-  
+
+  # Agents only
+  def list_channel_prformance(self, channel):
+    return self._request("/p/channels/performance.xml", channel)
+
+  # TOURS
   def search_tours(self, params = {}, channel = 0):
     if channel == 0:
       return self._request("/p/tours/search.xml", 0, params)
     else:
       return self._request("/c/tours/search.xml", channel, params)
-  
+
+  def show_tour(self, tour, channel):
+    return self._request("/c/tour/show.xml", channel, {"id": tour})
+
+  def tour_dates_and_deals(self, params = {}, tour = "", channel = 0):
+    return self._request("/c/tour/datesprices/datesndeals/search.xml", channel, {"id": tour})
+
+  # todo
+  # def update_tour(self, ):
+  #   return self._request("/c/tour/update.xml", channel, {"id": tour})
+
+  # todo
+  # list tour locations
+
+  # todo
+  # list product filters
+
   def search_hotels_range(self, params = {}, tour = "", channel = 0):
     params.update({"single_tour_id": tour})
     if channel == 0:
@@ -109,23 +141,56 @@ class Connection(object):
       return self._request("/p/tours/list.xml")
     else:
       return self._request("/c/tours/list.xml", channel)
-  
+
   def list_tour_images(self, channel = 0):
     if channel == 0:
       return self._request("/p/tours/images/list.xml")
     else:
       return self._request("/c/tours/images/list.xml", channel)
   
-  def show_tour(self, tour, channel):
-    return self._request("/c/tour/show.xml", channel, {"id": tour})
-  
   def show_tour_departures(self, tour, channel):
     return self._request("/c/tour/datesprices/dep/show.xml", channel, {"id": tour})
   
+  # todo: Managing dates & prices externally
+  # search_raw_departures
+  # show_departure
+  # create_new_departure
+  # update_departure
+  # delete_departure
+
+  # todo: possibly deprecated
   def show_tour_freesale(self, tour, channel):
     return self._request("/c/tour/datesprices/freesale/show.xml", channel, {"id": tour})
+
+  # todo:
+  # delete_tour
+  # upload_files
+  # delete_tour_image
+  # delete_tour_document
+
+
+  # Bookings
+  # todo: Creating bookings
+  # list_bookings
+  # search_bookings
+  # show_booking
+  # update_booking
+  # update_booking_component
+  # add_booking_component
+  # remove_booking_component
+  # add_note_to_booking
+  # send_booking_email
+  # cancel_booking
+
+  # todo:
+  # Payments
+  # Vouchers
+  # Customers & Enquiries
+  # Agents (Tour Operator use only)
+  # Internal Suppliers (Tour Operator use only)
 
   def show_supplier(self, supplier, channel):
     return self._request("/c/supplier/show.xml", channel, {"supplier_id": supplier})
 
-
+  # Staff Members (Tour Operator use only)
+  # Pickup Points (Tour Operator use only)
